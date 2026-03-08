5ac9ce9eb90af46fa605c164476dbc591fe23678
Ryosuke Canai <120649384+rc-code-jp@users.noreply.github.com>
2026-03-08 20:59:41 +0900

    Update minishelf.rb

--- Formula/minishelf.rb
+++ Formula/minishelf.rb
@@ -1,6 +1,6 @@
 class Minishelf < Formula
-    version "1.0.3"
-    sha256 "02eab83feeb573bae864d65f142d47b966637276b45d0a0d2746f1178ee21090"
+    version "1.0.4"
+    sha256 "57cc947f0e74684ab41a2b7481c7f55ef851ada3284cba268416369f61ba5bae"
 
     desc "Rust TUI file explorer with git-aware coloring"
     homepage "https://github.com/rc-code-jp/minishelf"
