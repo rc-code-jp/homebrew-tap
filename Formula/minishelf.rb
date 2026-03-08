4eda0dcab3eaa8873715c2828eefe16fe9cbefe9
Ryosuke Canai <120649384+rc-code-jp@users.noreply.github.com>
2026-03-08 11:54:27 +0900

    Update version and sha256 for Minishelf formula

--- Formula/minishelf.rb
+++ Formula/minishelf.rb
@@ -1,10 +1,11 @@
 class Minishelf < Formula
+    version "1.0.2"
+    sha256 "sha256:c3f1dd85bcbb2b3868fefb85fb354c8c099eed6dcac0470088c86c77b16fdb12"
+
     desc "Rust TUI file explorer with git-aware coloring"
     homepage "https://github.com/rc-code-jp/minishelf"
-    version "__VERSION__"
 
     url "https://github.com/rc-code-jp/minishelf/releases/download/v#{version}/minishelf-#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "__SHA256_AARCH64_APPLE_DARWIN__"
 
     def install
       bin.install "minishelf"
