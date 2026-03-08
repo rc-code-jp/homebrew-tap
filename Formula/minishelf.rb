d95ec604578bf6ebb2ef27a40d08dbbda2bcc167
Ryosuke Canai <120649384+rc-code-jp@users.noreply.github.com>
2026-03-08 12:05:14 +0900

    Fix sha256 format in minishelf.rb

--- Formula/minishelf.rb
+++ Formula/minishelf.rb
@@ -1,6 +1,6 @@
 class Minishelf < Formula
     version "1.0.3"
-    sha256 "sha256:02eab83feeb573bae864d65f142d47b966637276b45d0a0d2746f1178ee21090"
+    sha256 "02eab83feeb573bae864d65f142d47b966637276b45d0a0d2746f1178ee21090"
 
     desc "Rust TUI file explorer with git-aware coloring"
     homepage "https://github.com/rc-code-jp/minishelf"
